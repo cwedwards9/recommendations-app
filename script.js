7a3d14c1e01087b5cf644e2cd872a1a85bed930b
Chase Edwards <chase.w.edwards9@gmail.com>
2020-09-29 14:55:46 -0400

    Add loop to iterate through the object from the api ajax request and set each card in the html equal to each title that is sent back

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 // based on user input
 $("#searchBtn").on("click", function(){
     var title = $("#showInput").val();
-    var queryUrl = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + title + "&limit=1&type=show&k=385951-ChaseEdw-B7D7T5KF";
+    var queryUrl = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + title + "&limit=8&type=show&k=385951-ChaseEdw-B7D7T5KF";
     $.ajax({
       method: "GET",
       url: queryUrl
     })
     .done(function(data){
-        var movieTitle = data.Similar.Results[0].Name;
-        console.log(data);
-        $("#test").text(movieTitle);
+      for(var i = 0; i < 8; i++){
+        var movieTitle = data.Similar.Results[i].Name;
+        console.log(movieTitle);
+        $("#card" + i + " .card-title").text(movieTitle);
+      }
     });
 });
